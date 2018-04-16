434f64dda78683d5ffe177529941aefb8060638b
Hansen <hansenja24@gmail.com>
2018-04-16 05:02:07 -0700

    Change languages test

--- cypress-web-test/inside-web-test/cypress/integration/change-language_spec.js
+++ cypress-web-test/inside-web-test/cypress/integration/change-language_spec.js
@@ -1,9 +1,19 @@
-describe('Change Languages into Deutch', function () {
+//Changing Languages of the Content from Web App REACT
+//The purpose of this test to use the features of the Web App
+describe('Change Languages into Deutch, Vice Versa', function () {
   beforeEach(function () {
+    //Before each test, it will visit the localhost:3000 and select 'de' as Deutch language
     cy.visit('http://localhost:3000/')
+    cy.get('.EtUfl').select('de')
   })
 
-  it.only('hides "Clear Completed" with nothing checked', function () {
-    cy.get('.sc-htpNat').should('exist')
+  it.only('The Html Should Contain Deutch Paragraph or Sentences', function () {
+    cy.get('.kpbCLA').should('contain', 'Beginnen Sie Ihr nächstes React Projekt in Sekunden')
+    cy.get('.bPliuS').eq(1).click('center')
+    cy.get('.fyjucR').eq(4).should('contain', 'Komplette i18n Standard-Internationalisierung und Pluralisierung')
+
+    cy.get('.EtUfl').select('en')
+    cy.get('.fyjucR').eq(3).should('contain', 'Offline-first')
   })
+
 })
